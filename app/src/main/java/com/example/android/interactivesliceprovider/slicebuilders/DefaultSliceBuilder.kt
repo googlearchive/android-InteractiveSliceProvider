74aa8940f12bccf3eace706ffad087b2c76c373d
Jeremy Walker <jewalker@google.com>
2018-10-12 15:51:38 -0700

    Add detailed comments for slice fields.
    Bug: 112722740
    Test: Manually tested
    Change-Id: I50b3da19724a1ce271e3b7570edca700cf809f57

--- app/src/main/java/com/example/android/interactivesliceprovider/slicebuilders/DefaultSliceBuilder.kt
+++ app/src/main/java/com/example/android/interactivesliceprovider/slicebuilders/DefaultSliceBuilder.kt
@@ -34,7 +34,7 @@ class DefaultSliceBuilder(
 ) : SliceBuilder(sliceUri) {
 
     override fun buildSlice() = list(context, sliceUri, ListBuilder.INFINITY) {
-        val action = SliceAction.create(
+        val activityAction = SliceAction.create(
                 MainActivity.getPendingIntent(context),
                 IconCompat.createWithResource(context, R.mipmap.ic_launcher),
                 ListBuilder.LARGE_IMAGE,
@@ -42,28 +42,45 @@ class DefaultSliceBuilder(
         )
         return list(context, sliceUri, ListBuilder.INFINITY) {
             setAccentColor(ContextCompat.getColor(context, R.color.slice_accent_color))
+            // The first row can be a header or a row. If it's a header then it'll be styled
+            // slightly differently -- you might not always want the content in the top row to be
+            // bigger or differentiated if that doesn't make sense for the content.
             header {
                 title = "Default app launcher slice!"
-                subtitle = "Header subtitle"
-                summary = "Header Summary"
-                contentDescription = "Header Content Description"
-                primaryAction = action
+
+                // Subtitle is displayed when the slice is large (or small if a summary isn't set).
+                subtitle = "Subtitle displayed in large mode."
+
+                // Summary (optional) is displayed when the slice is small. It allows developers
+                // to provide a summary when the rest of the slice isn't visible (other rows
+                // outside of the header / first row aren't shown in small mode).
+                summary = "Summary displayed in small mode."
+
+                // Content description (optional) is what accessibility (talk back) will use to
+                // describe the slice when the header is focused. If this isn't set, talkback will
+                // read whatever text is in the header (title / subtitle etc.)
+                contentDescription = "Content Description used for accessibility."
+
+                primaryAction = activityAction
             }
+
+
+
             row {
                 subtitle = "Subtitle for row 2!"
                 contentDescription = "Row 2 Content Description"
                 // Places icon/action at the front of the slice.
                 // Note, these sorts of icons aren't allowed on the first row in a slice (usually
                 // reserved for the header).
-                setTitleItem(action)
-                primaryAction = action
+                setTitleItem(activityAction)
+                primaryAction = activityAction
                 }
             row {
                 title = "Try this third row!"
                 subtitle = "Subtitle for row 3!"
                 contentDescription = "Row 3 Content Description"
-                addEndItem(action)
-                primaryAction = action
+                addEndItem(activityAction)
+                primaryAction = activityAction
             }
         }
     }
